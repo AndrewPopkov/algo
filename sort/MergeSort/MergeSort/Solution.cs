2f6b64ea2905e4b8ff8662cb35cc9d488484d559
Андрей <Andypriestman@yandex.ru>
2016-12-20 20:16:26 +0300

    2

--- sort/MergeSort/MergeSort/Solution.cs
+++ sort/MergeSort/MergeSort/Solution.cs
@@ -8,38 +8,38 @@ namespace MergeSort
     class Solution
     {
 
-        int count = 0;
-        List<Element> rt = new List<Element>();
-        public int solution(int[] A)
-        {
-            Element[] Arr = new Element[A.Length];
+        //int count = 0;
+        //List<Element> rt = new List<Element>();
+        //public int solution(int[] A)
+        //{
+        //    Element[] Arr = new Element[A.Length];
 
-            for (int i = 0; i < A.Length; i++)
-            {
-                Arr[i] = new Element { point = i, radius = A[i] };
-            }
+        //    for (int i = 0; i < A.Length; i++)
+        //    {
+        //        Arr[i] = new Element { point = i, radius = A[i] };
+        //    }
 
-            Element[] result = MergeSort(Arr, 0, Arr.Length - 1);
+        //    Element[] result = MergeSort(Arr, 0, Arr.Length - 1);
 
-            return count;
+        //    return count;
 
-        }
+        //}
 
-        private T[] MergeSort(T[] A, int first, int last)
+       public static  T[] MergeSort(T[] A, int first, int last) where T : IEquatable<T>
         {
             if (first >= last)
             {
-                Element[] res = { A[first] };
+                T[] res = { A[first] };
                 return res;
             }
             int mid = first + (last - first) / 2;
-            Element[] leftArr = MergeSort(A, first, mid);
-            Element[] rightArr = MergeSort(A, mid + 1, last);
-            Element[] result = new Element[(leftArr.Length + rightArr.Length)];
+            T[] leftArr = T(A, first, mid);
+            T[] rightArr = T(A, mid + 1, last);
+            T[] result = new T[(leftArr.Length + rightArr.Length)];
             int i = 0, j = 0, k = 0;
             while (i < leftArr.Length && j < rightArr.Length)
             {
-                if (leftArr[i].radius < rightArr[j].radius)
+                if (leftArr[i] < rightArr[j])
                 {
                     if (((leftArr[i].point - leftArr[i].radius) <= (rightArr[j].point + rightArr[j].radius)) ||
                                         ((leftArr[i].point + leftArr[i].radius) >= (rightArr[j].point - rightArr[j].radius)))
